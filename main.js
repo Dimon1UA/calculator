f0306eea80010a60114b8c2711315d09efc3307e
Dimon1UA <92036555+Dimon1UA@users.noreply.github.com>
2022-01-17 22:05:55 +0200

    calculator

---
+++ main.js
@@ -0,0 +1,24 @@
+let name = prompt (`Ведіть своє ім'я`)
+alert (`Привіт, ` + name + `. Це мій перший калькулятор, і я хочу допомогти тобі вирішити приклад)`)
+let diya_1 = alert(`Давай додамо два числа`)
+let dodavannya_chislo_1 = Number(prompt (`Введіть перше число:`)) 
+let dodavannya_chislo_2 = Number(prompt (`Введіть друге число:`))
+alert (dodavannya_chislo_1+dodavannya_chislo_2);
+let diya_2 = alert(`Тепер віднімимо`)
+let dodavannya_chislo_3 = Number(prompt (`Введіть перше число:`)) 
+let dodavannya_chislo_4 = Number(prompt (`Введіть друге число:`))
+alert (dodavannya_chislo_3-dodavannya_chislo_4);
+let diya_3 = alert(`Спробуємо помножити`)
+let dodavannya_chislo_5 = Number(prompt (`Введіть перше число:`)) 
+let dodavannya_chislo_6 = Number(prompt (`Введіть друге число:`))
+alert (dodavannya_chislo_5*dodavannya_chislo_6);
+let diya_4 = alert(`Нумо поділимо два числа`)
+let dodavannya_chislo_7 = Number(prompt (`Введіть перше число:`)) 
+let dodavannya_chislo_8 = Number(prompt (`Введіть друге число:`))
+alert (dodavannya_chislo_7/dodavannya_chislo_8);
+let diya_5 = alert(`Піднесемо до степеня`)
+let dodavannya_chislo_9 = Number(prompt (`Введіть перше число:`)) 
+let dodavannya_chislo_10 = Number(prompt (`Введіть друге число:`))
+alert (dodavannya_chislo_9**dodavannya_chislo_10);
+let message = alert (`Дякую, що скористався моїм калькулятором)`)
+let a = alert (`Я хочу вивчати js` + `, після чого стану крутим програмістом!`)
